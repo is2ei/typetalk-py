e98a9da769c5531203271ae97986dc7894fd6fc3
Issei Horie <is2ei.horie@gmail.com>
2019-06-24 01:07:35 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as readme:
 
 setup(
     name="typetalk",
-    version="1.0.2",
+    version="1.0.3",
     packages=['typetalk'],
     description="Typetalk API client",
     long_description=long_description,
